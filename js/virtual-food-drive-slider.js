71f9aba6ae4b489e27255a2c13898e95c88514af
Cooper Brislain <cbrislain@gmail.com>
2019-02-02 20:22:49 -0800

    added lots of things

--- js/virtual-food-drive-slider.js
+++ js/virtual-food-drive-slider.js
@@ -1,15 +1,19 @@
-var multiplier = 1.85;
+var multiplier = 2.0;
+
+var regular_price = 1.85;
+var foodbank_price = 0.85;
 
 $(document).ready(function() {
     $('.slider').on('input',function() {
-        $('#price').val($(this).val());
+        var donation = $(this).val();
+        var quantity_at_regular_price = Math.round(donation/regular_price);
+        var quantity_at_foodbank_price = Math.round(donation/foodbank_price*multiplier);
+
         $('.itemcontainer').html('');
-        for(var i=0;i<$(this).val()*multiplier;i++) {
-            $('.itemcontainer').append('<span class="overlap'  + (i>$(this).val()? ' bonus': '') + '">&#x1F96B;</span>');
+        for(var i=0;i<quantity_at_foodbank_price;i++) {
+            $('.itemcontainer').append('<span class="overlap'  + (i>quantity_at_regular_price? ' bonus': '') + '"><image src="/img/' + (i>quantity_at_regular_price? 'bonus-': '') + 'grocery-bag.png"></span>');
         }
-        if($(this).val()%1.0) {
-            $('.itemcontainer').append('<span class="overlap partial'  + (i>$(this).val()*multiplier? ' bonus': '') + '" style="width: '+ (($(this).val()%1).toFixed(2)*12)+'px" ">&#x1f32e;</span>');
-        }
-        $('#quantity').val(($(this).val()*multiplier).toFixed(2));
+        $('.donationcontainer').html('An $' + donation + ' donation buys ' + quantity_at_foodbank_price + ' days of groceries');
+        $('<div class="smaller">').text('(' + quantity_at_regular_price + ' days at regular price)').appendTo('.donationcontainer');
     });
 });
